0f346a7141f2549d1df3b650c9e29fd3532c9c82
MHWillard <mhwillard@gmail.com>
2024-04-29 16:12:41 -0400

    daily

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -9,38 +9,35 @@ namespace WebApplication1
         public record Person(string FirstName, string LastName) { }
         public static void Main(string[] args)
         {
-
+            //1.Create a WebApplicationBuilder instance.
             WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
+            //2. Register the required services and configuration with the WebApplicationBuilder.
             builder.Services.AddHttpLogging(opts => opts.LoggingFields = HttpLoggingFields.RequestProperties);
             builder.Logging.AddFilter("Microsoft.AspNetCore.HttpLogging", LogLevel.Information);
 
+            //3. Call Build() on the builder instance to create a WebApplication instance.
             WebApplication app = builder.Build();
 
+            //4. Add middleware to the WebApplication to create a pipeline.
             if (app.Environment.IsDevelopment()) {
                 app.UseHttpLogging();
             };
 
+            //Adding more middleware in order to make a pipeline. 
+            app.UseWelcomePage("/");
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            //5. Map the endpoints in your application.
             app.MapGet("/", () => "Hello World!");
             app.MapGet("/person", () => new Person("Mister", "Johnson"));
 
+            //6. Call Run() on the WebApplication to start the server and handle requests.
             app.Run();
 
             
         }
     }
 }
-
-/*
- * ASP.NET Core entry point: 
-1. Create a WebApplicationBuilder instance.
-2. Register the required services and configuration with
-the WebApplicationBuilder.
-3. Call Build() on the builder instance to create a
-WebApplication instance.
-4. Add middleware to the WebApplication to
-create a pipeline.
-5. Map the endpoints in your application.
-6. Call Run() on the WebApplication to start
-the server and handle requests.
- */
